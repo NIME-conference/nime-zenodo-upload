bd7561eee15877a66489f9f2df1d2ae002f735fb
stefanofasciani <stefanofasciani@stefanofasciani.com>
2025-06-19 17:23:12 +0200

    2025 updates

--- nime_zenodo_upload/__main__.py
+++ nime_zenodo_upload/__main__.py
@@ -38,8 +38,8 @@ from pybtex.database.input import bibtex
 parser = bibtex.Parser()
 
 UPLOAD_FOLDER = './upload/'
-PUBLICATION_DATE = '2025-03-14'
-CONFERENCE_DATES = '4 September - 6 September, 2024'
+PUBLICATION_DATE = '2025-06-19'
+CONFERENCE_DATES = '24 June - 27 June, 2025'
 CONFERENCE_TITLE = 'International Conference on New Interfaces for Musical Expression'
 CONFERENCE_ACRONYM = 'NIME'
 
